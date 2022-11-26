ddae8d16a5753bfc0a4cc969b72a90bc58d1b356
MoetezKhemissi <khemissimoetez@gmail.com>
2022-11-26 23:18:07 +0100

    Unit Tests

--- Test/UnitTest.py
+++ Test/UnitTest.py
@@ -1,13 +1,18 @@
 from binance import Client, ThreadedWebsocketManager, ThreadedDepthCacheManager
 import datetime
-
-def fun(): 
-    pass
-client = Client('IasFiam7yUQneiVx1djOnUdz1vK4rKBlOobk6BkJFX762izFx9r3XBS2lUJT89F0','AU7lwvqsM10iE9BmQcJkU20fiI9ZQK8nhRidAsuNAeiBUjOxjBjiS55LuWp1VGG0',testnet=True)
-# Pinging two simultaneous pings to see time difference
-time1 =client.get_server_time()["serverTime"]
-time2 =client.get_server_time()["serverTime"]
-time3 = time2-time1
-# Convert Posix TimeStamp to time
-print(time3,"MS")
-fun()
+import unittest
+class TestLatency(unittest.TestCase):
+    def test_ping(self): 
+        client = Client('IasFiam7yUQneiVx1djOnUdz1vK4rKBlOobk6BkJFX762izFx9r3XBS2lUJT89F0','AU7lwvqsM10iE9BmQcJkU20fiI9ZQK8nhRidAsuNAeiBUjOxjBjiS55LuWp1VGG0',testnet=True)
+        # Pinging two simultaneous pings to see time difference
+        time1 =client.get_server_time()["serverTime"]
+        time2 =client.get_server_time()["serverTime"]
+        time3 = time2-time1
+        self.assertTrue(time3<5000,"Good ping")
+    def test_ping2(self): 
+        client2 = Client('IasFiam7yUQneiVx1djOnUdz1vK4rKBlOobk6BkJFX762izFx9r3XBS2lUJT89F0','AU7lwvqsM10iE9BmQcJkU20fiI9ZQK8nhRidAsuNAeiBUjOxjBjiS55LuWp1VGG0',testnet=True)
+        # Pinging two simultaneous pings to see time difference
+        time1 =client2.get_server_time()["serverTime"]
+        time2 =client2.get_server_time()["serverTime"]
+        time3 = time2-time1
+        self.assertTrue(time3<5000,"Good ping")
