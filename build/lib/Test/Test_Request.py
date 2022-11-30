15d5637d012f2c62c6b201b23cbb86b6b9db2aa2
MoetezKhemissi <70200510+MoetezKhemissi@users.noreply.github.com>
2022-11-30 01:08:35 +0100

    Unnecessary adds

--- build/lib/Test/Test_Request.py
+++
@@ -1,7 +0,0 @@
-
-import sys
- 
-from Python_bot import ApiKey
-#import urllib.request
-#url = "https://api.nomics.com/v1/orders/snapshot?key=your-key-here&exchange=binance&market=BTCUSDT&at=2019-04-01T12%3A00%3A00Z"
-#print(urllib.request.urlopen(url).read())
